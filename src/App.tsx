bf82a138c7157a1266024f0859076bf116f0dbbf
xabigarrido <xabigarrido@gmail.com>
2025-03-04 14:54:25 +0100

    vamossss22222223333s

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,28 @@
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
-import "leaflet/dist/leaflet.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [position, setPosition] = useState<LatLngExpression>([
-    37.7749, -122.4194,
-  ]); // Ubicación inicial (San Francisco)
-  const [loading, setLoading] = useState(true);
+  const [position, setPosition] = useState([37.7749, -122.4194]);
+  const [miData, setMiData] = useState({ name: "Xabier", subName: "Garrido" });
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition([pos.coords.latitude, pos.coords.longitude]);
-        setLoading(false);
-      },
-      () => {
-        alert("No se pudo obtener la ubicación");
-        setLoading(false);
-      }
-    );
-  }, []);
+  const handleButtonClick = () => {
+    // Construimos la URL con los datos que queremos enviar a la app
+    const data = `myapp://?name=${miData.name}&subName=${miData.subName}`;
+    window.location.href = data; // Esto enviará los datos a la app y la abrirá
+  };
 
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-      {loading ? (
-        <p>Cargando ubicación...</p>
-      ) : (
-        <MapContainer center={position} zoom={13} style={{ height: "100vh" }}>
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={position}>
-            <Popup>¡Estás aquí!</Popup>
-          </Marker>
-        </MapContainer>
-      )}
-      <button style={{ width: "100%", padding: 10 }}>Prueba xabi uoload</button>
+      <div
+        style={{
+          position: "absolute",
+          zIndex: 1221212,
+          height: 50,
+          width: "100%",
+          backgroundColor: "red",
+        }}
+      >
+        <button onClick={handleButtonClick}>Enviar Datos a la App</button>
+      </div>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Escucha en todas las interfaces de red
+    port: 5173, // El puerto que desees
+  },
+});
